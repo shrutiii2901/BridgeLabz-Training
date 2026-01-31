93ced414b6fd86791cea380b4bb01bed1b3faba4
shrutiii2901 <136559812+shrutiii2901@users.noreply.github.com>
2026-01-31 21:38:46 +0530

    [Shruti]added . adding regex-junit questions

---
+++ collections-practice/gcr-codebase/junit/src/main/java/StringUtils.java
@@ -0,0 +1,14 @@
+public class StringUtils {
+
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        return str.equals(reverse(str));
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
